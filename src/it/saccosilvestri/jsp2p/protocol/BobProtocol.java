a87b0f44a24d4697848a81bc637c64870fc59a5d
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2011-08-18 15:16:06 +0000

    naturalmente poi si fa la derivazione da Protocol classe base, ora si fa per provare

--- src/it/saccosilvestri/jsp2p/protocol/BobProtocol.java
+++ src/it/saccosilvestri/jsp2p/protocol/BobProtocol.java
@@ -50,11 +50,7 @@ public class BobProtocol {
 			InputStream in = clientSocket.getInputStream();
 			OutputStream out = clientSocket.getOutputStream();
 			
-			// (1) Invio del certificato del peer
-			byte[] certBytes = cert.getEncoded();
-			out.write(certBytes);
-			
-			// (2) Ricezione del certificato del peer, verifica ed estrazione della chiave pubblica.
+			// (1) Ricezione del certificato del peer, verifica ed estrazione della chiave pubblica.
 			byte[] certificate = null;
 			in.read(certificate);
 			CertificateFactory fact = CertificateFactory.getInstance("X.509","BC");			
@@ -68,36 +64,39 @@ public class BobProtocol {
 			System.out.println("Retrieving PublicKey...");
 			PublicKey pKey = retrievedCert.getPublicKey();
 			
-			// (3) Invio di un nonce cifrato con pKey
+			// (2) Invio del certificato del peer
+			byte[] certBytes = cert.getEncoded();
+			out.write(certBytes);
+			
+			// (3) Ricezione di nA
+			byte[] nA = new byte[1024/8];
+			in.read(nA);
+			byte[] nonceA = null;
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
+			cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+			nonceA = cipher.doFinal(nA);
+			
+			// (4) Invio di (nA,nB) cifrati con la chiave pubblica di A
 			// Create a secure random number generator
 			SecureRandom sr = SecureRandom.getInstance("SHA1PRNG","BC");
 			// Get 1024 random bits
-			byte[] nonceA = new byte[1024/8];
-			sr.nextBytes(nonceA);
+			byte[] nonceB = new byte[1024/8];
+			sr.nextBytes(nonceB);
 			byte[] cipherText = null;
-			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
 			// encrypt the plaintext using the public key
 			cipher.init(Cipher.ENCRYPT_MODE, pKey);
 			cipherText = cipher.doFinal(nonceA);
 			out.write(cipherText);
-			
-			// (4) Ricezione di (nA,nB) cifrati con la mia chiave pubblica
-			byte[] nA = new byte[1024/8];
-			byte[] nB = new byte[1024/8];
-			in.read(nA);
-			in.read(nB);
-			byte[] plainText = null;
-			cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
-			plainText = cipher.doFinal(nA);
-			if(!Arrays.equals(plainText,nonceA))
-				throw new BadNonceException();
-			plainText = cipher.doFinal(nB);
-			
-			
-			// (5) Invio di nB cifrato con la chiave pubblica di B
-			cipher.init(Cipher.ENCRYPT_MODE, pKey);
-			cipherText = cipher.doFinal(plainText);
+			cipherText = cipher.doFinal(nonceB);
 			out.write(cipherText);
+					
+			// (5) Ricezione e verifica di nB
+			byte[] nB = new byte[1024/8];
+			in.read(nB);
+			cipher.init(Cipher.DECRYPT_MODE, pKey);
+			byte[] plainText = cipher.doFinal(nB);
+			if(!Arrays.equals(plainText,nonceB))
+				throw new BadNonceException();
 			
 			
 			// Chiusura degli stream.
