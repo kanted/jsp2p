90b8db4621419ef3e904e5e744c55b967084713e
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2011-08-19 10:34:56 +0000

    testing

---
+++ trunk/src/it/saccosilvestri/jsp2p/test/Test.java
@@ -0,0 +1,12 @@
+package it.saccosilvestri.jsp2p.test;
+
+public class Test {
+
+	public static void main(String[] args)  {
+
+		System.out.println("Starting simulation");
+		// TODO Threads...
+		
+	}
+
+}
