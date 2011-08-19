6a8f2ef1a1287621993075281af98c35ea2e8af3
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2011-08-19 09:32:33 +0000

    bah

---
+++ trunk/src/it/saccosilvestri/jsp2p/securecommunication/Peer.java
@@ -0,0 +1,74 @@
+package it.saccosilvestri.jsp2p.securecommunication;
+
+import it.saccosilvestri.jsp2p.exceptions.BadNonceException;
+import it.saccosilvestri.jsp2p.protocol.AliceProtocol;
+import it.saccosilvestri.jsp2p.protocol.BobProtocol;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+
+import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
+
+public class Peer {
+	
+	private Socket clientSocket;
+	private Cipher cipher;
+	private Key sessionKey;
+	
+
+	public void send(byte[] messageToBeSent) throws InvalidKeyException {
+
+		cipher.init(Cipher.ENCRYPT_MODE, sessionKey);
+	}
+
+	public byte[] receive() throws InvalidKeyException {
+		cipher.init(Cipher.DECRYPT_MODE, sessionKey);
+		return null;
+
+	}
+
+	public Peer(boolean passive, Socket socket, KeyPair keyPair, X509Certificate peerCertificate, X509Certificate CACert) throws InvalidKeyException, CertificateException, SocketException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, IOException, BadNonceException {
+		clientSocket = socket;
+		// Basic validation
+		System.out.println("CA certificate:");
+		System.out.println("Validating dates...");
+		CACert.checkValidity(new Date());
+		System.out.println("Verifying signature...");
+		CACert.verify(CACert.getPublicKey());
+		System.out.println("Dates and signature verified.");
+		System.out.println("Retrieving PublicKey...");
+		PublicKey CAPublicKey = CACert.getPublicKey();
+		if(!passive){
+			AliceProtocol ap = new AliceProtocol(clientSocket,keyPair,peerCertificate,CAPublicKey);
+			sessionKey = ap.doService();
+		}
+		else{
+			BobProtocol bp = new BobProtocol(clientSocket,keyPair,peerCertificate,CAPublicKey);
+			sessionKey = bp.doService();
+		}
+		// Create the CipherStream to be used
+		System.out.println("Creating the CipherStream...");
+		cipher = Cipher.getInstance("TripleDES/ECB/PKCS1Padding",
+				"BC");
+
+	}
+}
