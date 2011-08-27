492acf8fe81aea1845311cbbd8ac608050f2d60d
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2011-08-27 14:00:53 +0000

    ora si prova

--- trunk/src/it/saccosilvestri/jsp2p/test/Peer.java
+++ trunk/src/it/saccosilvestri/jsp2p/test/Peer.java
@@ -5,51 +5,75 @@ import it.saccosilvestri.jsp2p.exceptions.UnreachableCAConfigurationFileExceptio
 import it.saccosilvestri.jsp2p.exceptions.WrongCAConfigurationFileSyntaxException;
 import it.saccosilvestri.jsp2p.securecommunication.SecureCommunication;
 import it.saccosilvestri.jsp2p.utility.ConfigurationFileUtility;
+import it.saccosilvestri.jsp2p.utility.FileUtility;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Properties;
 
 public class Peer {
 
-	private static int NUM_PEER;
-	private static int i;
-	private static KeyPair kp;
-	
-	public Peer(int i, KeyPair kp){
-		this.i=i;
-		this.kp=kp;
-	}
 
 	public static void main(String[] args) {
 
 		try {
 
 			int port = Integer.parseInt(args[0]);
-			if(port<1024||port>65535){
-				System.out.println("Attenzione. Inserire un numero di porta valido.");
+			if (port < 1024 || port > 65535) {
+				System.out
+						.println("Attenzione. Inserire un numero di porta valido.");
 				return;
 			}
-			
-			System.out.println("Starting peer "+i);
-			System.out.println("Recuperando il certificato per il peer "+i);
-			FileInputStream f = new FileInputStream("ca_certificate.crt"); //TODO magari path nel file di conf.
-			CertificateFactory fact = CertificateFactory.getInstance("X.509", "BC");
-			X509Certificate caCert = (X509Certificate) fact.generateCertificate(f);
-			f = new FileInputStream("certificate_for_peer_"+i);
+
+			// Recupero il numero del peer
+			int i = Integer.parseInt(args[1]);
+
+			System.out.println("Starting peer " + i);
+			System.out.println("Recuperando il certificato per il peer " + i);
+			FileInputStream f = new FileInputStream("ca_certificate.crt"); // TODO
+																			// magari
+																			// path
+																			// nel
+																			// file
+																			// di
+																			// conf.
+			CertificateFactory fact = CertificateFactory.getInstance("X.509",
+					"BC");
+			X509Certificate caCert = (X509Certificate) fact
+					.generateCertificate(f);
+			f = new FileInputStream("certificate_for_peer_" + i);
 			X509Certificate peerCert = (X509Certificate) fact
 					.generateCertificate(f);
+			System.out.println("Recuperando le chiavi per il peer " + i);
+			KeyPair kp = FileUtility.readKeysFromFiles("public" + i + ".key", "private" + i
+					+ ".key"); // TODO magari path nel file di conf.
+
 			// Un peer è sia alice che bob contemporaneamente.
-			AliceThread a = new AliceThread(peerCert, caCert, port, kp); 
+			AliceThread a = new AliceThread(peerCert, caCert, port, kp);
 			BobThread b = new BobThread(peerCert, caCert, port, kp);
 
 		} catch (Exception e) {
-			System.out.println("EXCEPTION: " + e.getClass() + " - " + e.getMessage());
+			System.out.println("EXCEPTION: " + e.getClass() + " - "
+					+ e.getMessage());
 		}
 	}
 
