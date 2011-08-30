8357f42b75b0b9d6f184aad2c89ea8d698cc0b05
cosimosacco@gmail.com <cosimosacco@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2011-08-30 09:03:31 +0000

    Ok Doxygen

---
+++ trunk/docs/html/navtree.js
@@ -0,0 +1,329 @@
+var NAVTREE =
+[
+  [ "jsp2p", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "it.saccosilvestri.jsp2p.protocol.AliceProtocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_alice_protocol.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.AliceThread", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_alice_thread.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.BadNonceException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_bad_nonce_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.protocol.BobProtocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_bob_protocol.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.BobThread", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_bob_thread.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.ByteArrayUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_byte_array_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.CertificateUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_certificate_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.certificationAuthority.CertificationAuthority", "classit_1_1saccosilvestri_1_1jsp2p_1_1certification_authority_1_1_certification_authority.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.ConfigurationFileUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_configuration_file_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.FileUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_file_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.logging.LogManager", "classit_1_1saccosilvestri_1_1jsp2p_1_1logging_1_1_log_manager.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.Peer", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_peer.html", null ],
+      [ "it.saccosilvestri.jsp2p.protocol.Protocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_protocol.html", null ],
+      [ "it.saccosilvestri.jsp2p.securecommunication.SecureCommunication", "classit_1_1saccosilvestri_1_1jsp2p_1_1securecommunication_1_1_secure_communication.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.Trent", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_trent.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.UnreachableCAConfigurationFileException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_unreachable_c_a_configuration_file_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.UnreachableLoggerConfigurationFileException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_unreachable_logger_configuration_file_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.WrongCAConfigurationFileSyntaxException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_wrong_c_a_configuration_file_syntax_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.WrongSubjectDNException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_wrong_subject_d_n_exception.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "it.saccosilvestri.jsp2p.test.AliceThread", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_alice_thread.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.BadNonceException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_bad_nonce_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.BobThread", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_bob_thread.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.ByteArrayUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_byte_array_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.CertificateUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_certificate_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.certificationAuthority.CertificationAuthority", "classit_1_1saccosilvestri_1_1jsp2p_1_1certification_authority_1_1_certification_authority.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.ConfigurationFileUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_configuration_file_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility.FileUtility", "classit_1_1saccosilvestri_1_1jsp2p_1_1utility_1_1_file_utility.html", null ],
+      [ "it.saccosilvestri.jsp2p.logging.LogManager", "classit_1_1saccosilvestri_1_1jsp2p_1_1logging_1_1_log_manager.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.Peer", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_peer.html", null ],
+      [ "it.saccosilvestri.jsp2p.protocol.Protocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_protocol.html", [
+        [ "it.saccosilvestri.jsp2p.protocol.AliceProtocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_alice_protocol.html", null ],
+        [ "it.saccosilvestri.jsp2p.protocol.BobProtocol", "classit_1_1saccosilvestri_1_1jsp2p_1_1protocol_1_1_bob_protocol.html", null ]
+      ] ],
+      [ "it.saccosilvestri.jsp2p.securecommunication.SecureCommunication", "classit_1_1saccosilvestri_1_1jsp2p_1_1securecommunication_1_1_secure_communication.html", null ],
+      [ "it.saccosilvestri.jsp2p.test.Trent", "classit_1_1saccosilvestri_1_1jsp2p_1_1test_1_1_trent.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.UnreachableCAConfigurationFileException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_unreachable_c_a_configuration_file_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.UnreachableLoggerConfigurationFileException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_unreachable_logger_configuration_file_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.WrongCAConfigurationFileSyntaxException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_wrong_c_a_configuration_file_syntax_exception.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions.WrongSubjectDNException", "classit_1_1saccosilvestri_1_1jsp2p_1_1exceptions_1_1_wrong_subject_d_n_exception.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Packages", "namespaces.html", [
+      [ "it", "namespaceit.html", null ],
+      [ "it::saccosilvestri", "namespaceit_1_1saccosilvestri.html", null ],
+      [ "it::saccosilvestri::jsp2p", "namespaceit_1_1saccosilvestri_1_1jsp2p.html", null ],
+      [ "it.saccosilvestri.jsp2p.certificationAuthority", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1certification_authority.html", null ],
+      [ "it.saccosilvestri.jsp2p.exceptions", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1exceptions.html", null ],
+      [ "it.saccosilvestri.jsp2p.logging", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1logging.html", null ],
+      [ "it.saccosilvestri.jsp2p.protocol", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1protocol.html", null ],
+      [ "it.saccosilvestri.jsp2p.securecommunication", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1securecommunication.html", null ],
+      [ "it.saccosilvestri.jsp2p.test", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1test.html", null ],
+      [ "it.saccosilvestri.jsp2p.utility", "namespaceit_1_1saccosilvestri_1_1jsp2p_1_1utility.html", null ]
+    ] ],
+    [ "File List", "files.html", [
+      [ "src/it/saccosilvestri/jsp2p/certificationAuthority/CertificationAuthority.java", "_certification_authority_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/exceptions/BadNonceException.java", "_bad_nonce_exception_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/exceptions/UnreachableCAConfigurationFileException.java", "_unreachable_c_a_configuration_file_exception_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/exceptions/UnreachableLoggerConfigurationFileException.java", "_unreachable_logger_configuration_file_exception_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/exceptions/WrongCAConfigurationFileSyntaxException.java", "_wrong_c_a_configuration_file_syntax_exception_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/exceptions/WrongSubjectDNException.java", "_wrong_subject_d_n_exception_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/logging/LogManager.java", "_log_manager_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/protocol/AliceProtocol.java", "_alice_protocol_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/protocol/BobProtocol.java", "_bob_protocol_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/protocol/Protocol.java", "_protocol_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/securecommunication/SecureCommunication.java", "_secure_communication_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/test/AliceThread.java", "_alice_thread_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/test/BobThread.java", "_bob_thread_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/test/Peer.java", "_peer_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/test/Trent.java", "_trent_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/utility/ByteArrayUtility.java", "_byte_array_utility_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/utility/CertificateUtility.java", "_certificate_utility_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/utility/ConfigurationFileUtility.java", "_configuration_file_utility_8java.html", null ],
+      [ "src/it/saccosilvestri/jsp2p/utility/FileUtility.java", "_file_utility_8java.html", null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
