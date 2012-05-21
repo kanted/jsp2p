9009959c625e6a1da32fbcf84d70035b2eda4e50
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2012-05-21 09:33:19 +0000

    cosimo

--- tcpsg-0.0.2/common.c
+++ tcpsg-0.0.2/common.c
@@ -90,3 +90,33 @@ void destroy_ctx(ctx)
   {
     SSL_CTX_free(ctx);
   }
+
+void load_dh_params(ctx,file)
+  SSL_CTX *ctx;
+  char *file;
+  {
+    DH *ret=0;
+    BIO *bio;
+
+    if ((bio=BIO_new_file(file,"r")) == NULL)
+      berr_exit("Couldn't open DH file");
+
+    ret=PEM_read_bio_DHparams(bio,NULL,NULL,
+      NULL);
+    BIO_free(bio);
+    if(SSL_CTX_set_tmp_dh(ctx,ret)<0)
+      berr_exit("Couldn't set DH parameters");
+  }
+
+void generate_eph_rsa_key(ctx)
+  SSL_CTX *ctx;
+  {
+    RSA *rsa;
+
+    rsa=RSA_generate_key(512,RSA_F4,NULL,NULL);
+    
+    if (!SSL_CTX_set_tmp_rsa(ctx,rsa))
+      berr_exit("Couldn't set RSA key");
+
+    RSA_free(rsa);
+  }
