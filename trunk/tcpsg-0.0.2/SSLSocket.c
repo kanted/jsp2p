980457c0f99aed547dbd1cdf7bd147f125ef06dc
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2012-05-24 14:50:44 +0000

    sslsocket

--- trunk/tcpsg-0.0.2/SSLSocket.c
+++ trunk/tcpsg-0.0.2/SSLSocket.c
@@ -18,8 +18,10 @@ SSLSocket* SSL_socket(int baseSocket, char* keyFile, char* password)
     method = SSLv23_method();
     SSLSocket* secureSocket = malloc(sizeof(SSLSocket));
     secureSocket->ctx = SSL_CTX_new(method);
-    if(!(SSL_CTX_use_certificate_chain_file(secureSocket->ctx, keyFile)))
+    if(!(SSL_CTX_use_certificate_chain_file(secureSocket->ctx, keyFile))){
+        printf("Error reading certificate\n"); //TODO
         goto exceptionHandler;
+    }
     staticPassword = password;
     SSL_CTX_set_default_passwd_cb(secureSocket->ctx, passwordCopy);
     if
@@ -27,7 +29,11 @@ SSLSocket* SSL_socket(int baseSocket, char* keyFile, char* password)
         !(SSL_CTX_use_PrivateKey_file(secureSocket->ctx, keyFile, SSL_FILETYPE_PEM))
         ||
         !(SSL_CTX_load_verify_locations(secureSocket->ctx, CA_LIST, 0))
-    ) goto exceptionHandler;
+    )
+    {
+        printf("Error reading private key\n"); //TODO
+        goto exceptionHandler;
+    }
     //load_dh_params(secureSocket->ctx, main_opt.dhfile);
     sbio = BIO_new_socket(baseSocket, BIO_NOCLOSE);
     secureSocket->ssl = SSL_new(secureSocket->ctx);
