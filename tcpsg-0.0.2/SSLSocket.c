90cfd377204df8ccc3743a33637ba9feccf81ba3
davidesil.web@gmail.com <davidesil.web@gmail.com@52281bc3-0f63-ad7e-736a-7c3d5d123d18>
2012-05-24 14:39:24 +0000

    sslsocket

--- tcpsg-0.0.2/SSLSocket.c
+++ tcpsg-0.0.2/SSLSocket.c
@@ -15,10 +15,10 @@ SSLSocket* SSL_socket(int baseSocket, char* keyFile, char* password)
     BIO* sbio;
     SSL_library_init();
     SSL_load_error_strings();
-    meth = SSLv23_method();
+    method = SSLv23_method();
     SSLSocket* secureSocket = malloc(sizeof(SSLSocket));
     secureSocket->ctx = SSL_CTX_new(method);
-    if(!(SSL_CTX_use_certificate_chain_file(secureSocket->ctx, keyfile)))
+    if(!(SSL_CTX_use_certificate_chain_file(secureSocket->ctx, keyFile)))
         goto exceptionHandler;
     staticPassword = password;
     SSL_CTX_set_default_passwd_cb(secureSocket->ctx, passwordCopy);
@@ -26,10 +26,10 @@ SSLSocket* SSL_socket(int baseSocket, char* keyFile, char* password)
     (
         !(SSL_CTX_use_PrivateKey_file(secureSocket->ctx, keyfile, SSL_FILETYPE_PEM))
         ||
-        !(SSL_CTX_load_verify_locations(*ctx, CA_LIST, 0))
+        !(SSL_CTX_load_verify_locations(secureSocket->ctx, CA_LIST, 0))
     ) goto exceptionHandler;
     //load_dh_params(secureSocket->ctx, main_opt.dhfile);
-    sbio = BIO_new_socket(client_sockfd, BIO_NOCLOSE);
+    sbio = BIO_new_socket(baseSocket, BIO_NOCLOSE);
     secureSocket->ssl = SSL_new(secureSocket->ctx);
     SSL_set_bio(secureSocket->ssl, sbio, sbio);
     return secureSocket;
@@ -43,6 +43,6 @@ void SSL_close(SSLSocket* secureSocket)
 {
     SSL_shutdown(secureSocket->ssl);
     SSL_free(secureSocket->ssl);
-    destroy_ctx(secureSocket->ctx);
+    SSL_CTX_free(secureSocket->ctx);
     free(secureSocket);
 }
